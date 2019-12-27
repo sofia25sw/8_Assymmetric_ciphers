855d3d70b2cdeec213f691bee69bfcb3f0c688c8
sonya <sofarogoznikova598@gmail.com>
2019-12-27 16:51:16 +0300

    vse

--- client.py
+++ client.py
@@ -1,5 +1,19 @@
 import socket
 import pickle
+from random import randint
+
+def cript(a1, key):
+    a2 = [chr(ord(a1[i]) ^ key) for i in range(len(a1))]
+    return ''.join(a2)
+
+def sen(sock, msg, K):
+    msg = cript(msg, K)
+    sock.send(pickle.dumps(msg))
+
+def rec(sock, K):
+    msg = pickle.loads(sock.recv(1024))
+    msg = cript(msg,K)
+    return msg
 
 HOST = '127.0.0.1'
 PORT = 8080
@@ -7,8 +21,18 @@ PORT = 8080
 sock = socket.socket()
 sock.connect((HOST, PORT))
 
-p, g, a = 7, 5, 3
+p, g, a = [randint(0,250) for i in range(3)]
 A = g ** a % p
 sock.send(pickle.dumps((p, g, A)))
+B = pickle.loads(sock.recv(1024))
+K = B ** a % p
+
+print('Введите сообщение')
+msg = input()
+
+while msg != 'exit':
+    sen(sock,msg, K)
+    print(rec(sock,K))
+    msg = input()
 
 sock.close()
--- server.py
+++ server.py
@@ -1,15 +1,40 @@
 import socket
 import pickle
+from random import randint
+
+def cript(a1, key):
+    a2 = [chr(ord(a1[i]) ^ key) for i in range(len(a1))]
+    return ''.join(a2)
+
+def sen(conn, msg, K):
+    msg = cript(msg, K)
+    conn.send(pickle.dumps(msg))
+
+def rec(conn, K):
+    msg = pickle.loads(conn.recv(1024))
+    msg = cript(msg, K)
+    return msg
 
 HOST = '127.0.0.1'
 PORT = 8080
-
 sock = socket.socket()
 sock.bind((HOST, PORT))
 sock.listen(1)
 conn, addr = sock.accept()
 
 msg = conn.recv(1024)
-print(pickle.loads(msg))
+p, g, A = pickle.loads(msg)
+b = randint(10, 250)
+B = g ** b % p
+conn.send(pickle.dumps(B))
+K = A ** b % p
+
+while True:
+    try:
+        msg = rec(conn, K)
+        print(msg)
+        sen(conn, msg, K)
+    except EOFError:
+        break
 
 conn.close()
